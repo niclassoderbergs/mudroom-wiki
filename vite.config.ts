0503f95bdd65aa515fb02a9ed255aad89414e559
niclassoderbergs <niclas@konvene.se>
2025-12-28 23:29:32 +0100

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // Vi tar bort "base: './'" eftersom Vercel/Netlify hanterar detta automatiskt vid roten.
-})
